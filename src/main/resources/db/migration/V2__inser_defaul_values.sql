1e197d1cc111ec1db282718304ffc0bb2409885d
tima <timurshvahev977@gmail.com>
2022-07-02 23:10:06 +0600

    больше данных в бд

--- src/main/resources/db/migration/V2__inser_defaul_values.sql
+++ src/main/resources/db/migration/V2__inser_defaul_values.sql
@@ -5,16 +5,40 @@ values (1, 'COMEDIAN'),
        (4, 'ADVENTURE');
 
 insert into author(id, name)
-values (1, 'A.S.Pushkin');
+values (1, 'A.S.Pushkin'),
+       (2, 'Михаил Булгаков'),
+       (3, 'Лев Толстой'),
+       (4, 'Александр Грибоедов'),
+       (5, 'Александр Куприн'),
+       (6, 'A.S.Pushkin1'),
+       (7, 'A.S.Pushkin2');
 
 insert into book(id, name, price)
-values (1, 'Сказка о царе Салтане', 24);
+values (1, 'Сказка о царе Салтане', 241),
+       (2, 'Роковые яйца', 242),
+       (3, 'Война и мир', 243),
+       (4, 'Горе от ума', 244),
+       (5, 'Чудесный мир', 245);
 
 insert into author_genre(author_id, genre_id)
-values (1, 1);
+values (1, 1),
+       (2, 3),
+       (3, 4),
+       (4, 2),
+       (5, 1),
+       (6, 2),
+       (7, 3);
 
 insert into book_genre(genre_id, book_id)
-values (1, 1);
+values (1, 1),
+       (2, 3),
+       (3, 4),
+       (4, 2),
+       (5, 1);
 
 insert into book_author(author_id, book_id)
-values (1, 1);
+values (1, 1),
+       (2, 2),
+       (3, 3),
+       (4, 4),
+       (5, 5);
