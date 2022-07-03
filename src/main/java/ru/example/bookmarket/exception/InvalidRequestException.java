90578338e068cb57079072670a0adaa388668943
tima <timurshvahev977@gmail.com>
2022-07-03 14:47:15 +0600

    добавлена @ResponseStatus

--- src/main/java/ru/example/bookmarket/exception/InvalidRequestException.java
+++ src/main/java/ru/example/bookmarket/exception/InvalidRequestException.java
@@ -2,13 +2,12 @@ package ru.example.bookmarket.exception;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.nio.charset.Charset;
-
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class InvalidRequestException extends RuntimeException {
-
-
     public InvalidRequestException(String message) {
         super(message);
     }
