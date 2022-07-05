e7aa2f38147942e02d9e936f07e2cfd4cbb6b449
tima <timurshvahev977@gmail.com>
2022-07-05 15:10:10 +0600

    еще чуть чуть

--- src/main/java/ru/example/bookmarket/dto/AuthorDTO.java
+++ src/main/java/ru/example/bookmarket/dto/AuthorDTO.java
@@ -1,5 +1,6 @@
 package ru.example.bookmarket.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import ru.example.bookmarket.genre.Genre;
@@ -7,8 +8,11 @@ import ru.example.bookmarket.genre.Genre;
 import java.util.Set;
 @Data
 @Builder
+@Schema
 public class AuthorDTO {
+    @Schema(type = "string", example = " ")
     private Long id;
+    @Schema(type = "string", example = " ")
     private String name;
     private Set<Genre> genres;
 
--- src/main/java/ru/example/bookmarket/dto/BookDTO.java
+++ src/main/java/ru/example/bookmarket/dto/BookDTO.java
@@ -1,5 +1,6 @@
 package ru.example.bookmarket.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import ru.example.bookmarket.genre.Genre;
@@ -9,10 +10,14 @@ import java.util.Set;
 
 @Data
 @Builder
+@Schema
 public class BookDTO  {
+    @Schema(type = "string", example = " ")
     private Long id;
+    @Schema(type = "string", example = " ")
     private String name;
     private Set<AuthorDTO> authors;
     private Set<Genre> genres;
+    @Schema(type = "string", example = " ")
     private Integer price;
 }
