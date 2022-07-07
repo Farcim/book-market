6c27295c33c5a67799fa1b2546df5b15b2c02c08
tima <timurshvahev977@gmail.com>
2022-07-07 13:09:51 +0600

    page default

--- src/main/java/ru/example/bookmarket/controller/AuthorController.java
+++ src/main/java/ru/example/bookmarket/controller/AuthorController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
